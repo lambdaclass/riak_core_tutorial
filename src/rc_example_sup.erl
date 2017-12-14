304b33aff78c9d93ad146cc2c6ee1cb133eccc0a
Facundo Olano <facundo.olano@gmail.com>
2017-12-14 12:55:29 -0300

    Ping working

--- src/rc_example_sup.erl
+++ src/rc_example_sup.erl
@@ -1,8 +1,3 @@
-%%%-------------------------------------------------------------------
-%% @doc rc_example top level supervisor.
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(rc_example_sup).
 
 -behaviour(supervisor).
@@ -15,21 +10,14 @@
 
 -define(SERVER, ?MODULE).
 
-%%====================================================================
-%% API functions
-%%====================================================================
-
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%%====================================================================
-%% Supervisor callbacks
-%%====================================================================
-
-%% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, []} }.
+  VMaster = {rc_example_vnode_master,
+             {riak_core_vnode_master, start_link, [rc_example_vnode]},
+             permanent, 5000, worker, [riak_core_vnode_master]},
 
-%%====================================================================
-%% Internal functions
-%%====================================================================
+  {ok,
+   {{one_for_one, 5, 10},
+    [VMaster]}}.
