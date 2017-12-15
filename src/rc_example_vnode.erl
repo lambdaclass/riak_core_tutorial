d1fd95b022ace3282de588fe77a838a85071919b
Facundo Olano <facundo.olano@gmail.com>
2017-12-15 16:51:17 -0300

    use maps instead of records, fixes #1

--- src/rc_example_vnode.erl
+++ src/rc_example_vnode.erl
@@ -16,23 +16,18 @@
          handle_coverage/4,
          handle_exit/3]).
 
--ignore_xref([
-              start_vnode/1
-             ]).
-
--record(state, {partition}).
-
 %% API
 start_vnode(I) ->
   riak_core_vnode_master:get_vnode_pid(I, ?MODULE).
 
 init([Partition]) ->
-  {ok, #state { partition=Partition }}.
+  {ok, #{partition => Partition}}.
 
 %% Sample command: respond to a ping
-handle_command(ping, _Sender, State) ->
-  lager:info("Reived ping comand ~p", [State#state.partition]),
-  {reply, {pong, State#state.partition}, State};
+handle_command(ping, _Sender, #{partition := Partition} = State) ->
+  lager:info("Reived ping comand ~p", [Partition]),
+  {reply, {pong, Partition}, State};
+
 handle_command(Message, _Sender, State) ->
   lager:warning("unhandled_command ~p", [Message]),
   {noreply, State}.
