70494031ff959857de2041e86371b714c7901b9a
Francisco Krause Arnim <fkrausear@gmail.com>
2022-06-03 16:40:32 -0300

    Apply formatting

--- src/rc_example_coverage_fsm.erl
+++ src/rc_example_coverage_fsm.erl
@@ -2,51 +2,54 @@
 
 -behaviour(riak_core_coverage_fsm).
 
--export([start_link/4,
-         init/2,
-         process_results/2,
-         finish/2]).
+-export([start_link/4, init/2, process_results/2, finish/2]).
 
 start_link(ReqId, ClientPid, Request, Timeout) ->
-  riak_core_coverage_fsm:start_link(?MODULE, {pid, ReqId, ClientPid}, [Request, Timeout]).
+    riak_core_coverage_fsm:start_link(?MODULE, {pid, ReqId, ClientPid}, [Request, Timeout]).
 
 %% riak_core_coverage_fsm API
 
 init({pid, ReqId, ClientPid}, [Request, Timeout]) ->
-  logger:info("Starting coverage request ~p ~p", [ReqId, Request]),
+    logger:info("Starting coverage request ~p ~p", [ReqId, Request]),
 
-  State = #{req_id => ReqId,
-            from => ClientPid,
-            request => Request,
-            accum => []},
+    State =
+        #{req_id => ReqId,
+          from => ClientPid,
+          request => Request,
+          accum => []},
 
-  %% See https://github.com/Kyorai/riak_core/blob/3.0.9/src/riak_core_coverage_fsm.erl#L45-L63
-  %% for details on each of these elements
-  {Request, %% An opaque data structure that is used by the VNode to implement the specific coverage request.
-   allup,  %% 'all' for all VNodes or 'allup' for all reachable.
-   1, %% replication factor
-   1, %%  number of primary VNodes from the preference list to use
-   rc_example, %% service to use to check for available nodes
-   rc_example_vnode_master, %% The atom to use to reach the vnode master module
-   Timeout,
-   State}.
+    %% See https://github.com/Kyorai/riak_core/blob/3.0.9/src/riak_core_coverage_fsm.erl#L45-L63
+    %% for details on each of these elements
+    {Request, %% An opaque data structure that is used by the VNode to implement the specific coverage request.
+     allup,  %% 'all' for all VNodes or 'allup' for all reachable.
+     1, %% replication factor
+     1, %%  number of primary VNodes from the preference list to use
+     rc_example, %% service to use to check for available nodes
+     rc_example_vnode_master, %% The atom to use to reach the vnode master module
+     Timeout,
+     State}.
 
-process_results({{_ReqId, {_Partition, _Node}}, []}, State ) ->
-  {done, State};
+process_results({{_ReqId, {_Partition, _Node}}, []}, State) ->
+    {done, State};
+process_results({{_ReqId, {Partition, Node}}, Data}, State = #{accum := Accum}) ->
+    NewAccum = [{Partition, Node, Data} | Accum],
+    {done, State#{accum => NewAccum}}.
 
-process_results({{_ReqId, {Partition, Node}}, Data},
-                State = #{accum := Accum}) ->
-  NewAccum = [{Partition, Node, Data} | Accum],
-  {done, State#{accum => NewAccum}}.
+finish(clean,
+       State =
+           #{req_id := ReqId,
+             from := From,
+             accum := Accum}) ->
+    logger:info("Finished coverage request ~p", [ReqId]),
 
-finish(clean, State = #{req_id := ReqId, from := From, accum := Accum}) ->
-  logger:info("Finished coverage request ~p", [ReqId]),
-
-  %% send the result back to the caller
-  From ! {ReqId, {ok, Accum}},
-  {stop, normal, State};
-
-finish({error, Reason}, State = #{req_id := ReqId, from := From, accum := Accum}) ->
-  logger:warning("Coverage query failed! Reason: ~p", [Reason]),
-  From ! {ReqId, {partial, Reason, Accum}},
-  {stop, normal, State}.
+    %% send the result back to the caller
+    From ! {ReqId, {ok, Accum}},
+    {stop, normal, State};
+finish({error, Reason},
+       State =
+           #{req_id := ReqId,
+             from := From,
+             accum := Accum}) ->
+    logger:warning("Coverage query failed! Reason: ~p", [Reason]),
+    From ! {ReqId, {partial, Reason, Accum}},
+    {stop, normal, State}.
--- src/rc_example_coverage_fsm_sup.erl
+++ src/rc_example_coverage_fsm_sup.erl
@@ -2,19 +2,21 @@
 
 -behavior(supervisor).
 
--export([start_link/0,
-         start_fsm/1,
-         init/1]).
+-export([start_link/0, start_fsm/1, init/1]).
 
 start_link() ->
-  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-  CoverageFSM = {undefined,
-                 {rc_example_coverage_fsm, start_link, []},
-                 temporary, 5000, worker, [rc_example_coverage_fsm]},
+    CoverageFSM =
+        {undefined,
+         {rc_example_coverage_fsm, start_link, []},
+         temporary,
+         5000,
+         worker,
+         [rc_example_coverage_fsm]},
 
-  {ok, {{simple_one_for_one, 10, 10}, [CoverageFSM]}}.
+    {ok, {{simple_one_for_one, 10, 10}, [CoverageFSM]}}.
 
 start_fsm(Args) ->
-  supervisor:start_child(?MODULE, Args).
+    supervisor:start_child(?MODULE, Args).
